66b1c38de5c09b6915d6eec46c879c0d340998e6
Kosuke Hoshi <kousuke.1120.hoshi@gmail.com>
2022-06-18 14:10:10 +0900

    templateを別に用意

---
+++ src/_/template/index.ts
@@ -0,0 +1,22 @@
+import * as readline from "readline";
+
+const main = (argss: string[][]) => {
+  const A = +argss[0][0];
+  console.log(A);
+};
+
+// ----------------------------------------------------------------------------
+// Template
+
+const __lines: string[] = [];
+const __reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+__reader.on('line', (line) => {
+  __lines.push(line);
+});
+__reader.on('close', () => {
+  main(__lines.map(s => s.split(' ')));
+});
